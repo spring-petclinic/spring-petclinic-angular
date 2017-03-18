f948f5fe72f35e960c36e8dc7e20c6c9bbbce369
Vitaliy Fedoriv <vitaliy.fedoriv@gmail.com>
2017-03-18 16:36:54 +0200

    add specialty-add.component

---
+++ src/app/specialties/specialty-add/specialty-add.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SpecialtyAddComponent } from './specialty-add.component';
+
+describe('SpecialtyAddComponent', () => {
+  let component: SpecialtyAddComponent;
+  let fixture: ComponentFixture<SpecialtyAddComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ SpecialtyAddComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SpecialtyAddComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/specialties/specialty-add/specialty-add.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-specialty-add',
+  templateUrl: './specialty-add.component.html',
+  styleUrls: ['./specialty-add.component.css']
+})
+export class SpecialtyAddComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
