3511433ad21a09beea08bb4bafffe900b7d4e431
Vitaliy Fedoriv <vitaliy.fedoriv@gmail.com>
2017-04-03 23:13:01 +0300

    add datepicker component in visit-add component

--- src/app/visits/visit-add/visit-add.component.ts
+++ src/app/visits/visit-add/visit-add.component.ts
@@ -36,6 +36,52 @@ import {Owner} from '../../owners/owner';
   styleUrls: ['./visit-add.component.css']
 })
 export class VisitAddComponent implements OnInit {
+
+  // just copy of demo datepicker - changes needed
+
+  isRequired = false;
+  isDisabled = false;
+  isOpenOnFocus = false;
+  isOpen = false;
+  today: Date = new Date();
+  type: string = 'date';
+  types: Array<any> = [
+    { text: 'Date', value: 'date' },
+    { text: 'Time', value: 'time' },
+    { text: 'Date Time', value: 'datetime' }];
+
+  mode: string = 'auto';
+  modes: Array<any> = [
+    { text: 'Auto', value: 'auto' },
+    { text: 'Portrait', value: 'portrait' },
+    { text: 'Landscape', value: 'landscape' }];
+
+  container: string = 'inline';
+  containers: Array<any> = [
+    { text: 'Inline', value: 'inline' },
+    { text: 'Dialog', value: 'dialog' }];
+
+  date: Date = null;
+  minDate: Date = null;
+  maxDate: Date = null;
+  enableDates: Array<Date> = [
+    new Date(this.today.getFullYear(), this.today.getMonth(), this.today.getDate() - 7),
+    new Date(this.today.getFullYear(), this.today.getMonth(), this.today.getDate() - 1),
+    new Date(this.today.getFullYear(), this.today.getMonth(), this.today.getDate() + 5),
+    new Date(this.today.getFullYear(), this.today.getMonth(), this.today.getDate() + 7),
+    new Date(this.today.getFullYear(), this.today.getMonth(), this.today.getDate() + 8)
+  ];
+  disableDates: Array<Date> = [
+    new Date(this.today.getFullYear(), this.today.getMonth(), this.today.getDate() - 2),
+    new Date(this.today.getFullYear(), this.today.getMonth(), this.today.getDate() - 1),
+    new Date(this.today.getFullYear(), this.today.getMonth(), this.today.getDate() + 2),
+    new Date(this.today.getFullYear(), this.today.getMonth(), this.today.getDate() + 5),
+    new Date(this.today.getFullYear(), this.today.getMonth(), this.today.getDate() + 9)
+  ];
+  disableWeekDays: Array<number> = [0, 6];
+
+  //  end copy datepicker demo
+
   visit: Visit;
   current_pet: Pet;
   current_owner: Owner;
