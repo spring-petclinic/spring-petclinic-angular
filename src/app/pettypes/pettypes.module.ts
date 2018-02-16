95324a88763259bf725edaa4472af1e0f7a60f03
Vitaliy Fedoriv <vitaliy.fedoriv@gmail.com>
2018-02-16 11:57:39 +0200

    update pettypes module

--- src/app/pettypes/pettypes.module.ts
+++ src/app/pettypes/pettypes.module.ts
@@ -24,13 +24,16 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {FormsModule} from '@angular/forms';
 import {PetTypeService} from './pettype.service';
+import { PettypeListComponent } from './pettype-list/pettype-list.component';
+import { PettypeAddComponent } from './pettype-add/pettype-add.component';
+import { PettypeEditComponent } from './pettype-edit/pettype-edit.component';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule
   ],
-  declarations: [],
+  declarations: [PettypeListComponent, PettypeAddComponent, PettypeEditComponent],
   providers: [PetTypeService]
 })
 export class PetTypesModule {
