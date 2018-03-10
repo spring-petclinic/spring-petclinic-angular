387ac48ae2edd44325e1baeb01ff46b1006b9e86
Vitaliy Fedoriv <vitaliy.fedoriv@gmail.com>
2018-03-10 16:02:36 +0200

    update Vet service

--- src/app/vets/vet.service.ts
+++ src/app/vets/vet.service.ts
@@ -22,9 +22,10 @@
 
 import {Injectable} from '@angular/core';
 import {environment} from '../../environments/environment';
-import {Http, Response} from '@angular/http';
+import {Headers, Http, RequestOptions, Response} from '@angular/http';
 import {Observable} from 'rxjs';
 import {Vet} from './vet';
+
 
 @Injectable()
 export class VetService {
@@ -37,6 +38,37 @@ export class VetService {
   getVets(): Observable<Vet[]> {
     return this._http.get(this.entity_url)
       .map((response: Response) => <Vet[]> response.json())
+      .catch(this.handleError);
+  }
+
+  getVetById(vet_id: string): Observable<Vet> {
+    return this._http.get((this.entity_url + '/' + vet_id))
+      .map((response: Response) => <Vet> response.json())
+      .catch(this.handleError);
+  }
+
+  updateVet(vet_id: string, vet: Vet): Observable<Vet> {
+    const body = JSON.stringify(vet);
+    const headers = new Headers({'Content-Type': ' application/json;charset=UTF-8'});
+    const options = new RequestOptions({headers: headers});
+    return this._http.put((this.entity_url + '/' + vet_id), body, options)
+      .map((response: Response) => response)
+      .catch(this.handleError);
+  }
+
+  addVet(vet: Vet): Observable<Vet> {
+    const headers = new Headers();
+    const body = JSON.stringify(vet);
+    headers.append('Content-Type', 'application/json');
+    headers.append('Accept', 'application/json');
+    return this._http.post(this.entity_url, body, {headers})
+      .map((response: Response) => <Vet> response.json())
+      .catch(this.handleError);
+  }
+
+  deleteVet(vet_id: string): Observable<number> {
+    return this._http.delete(this.entity_url + '/' + vet_id)
+      .map((response: Response) => response.status)
       .catch(this.handleError);
   }
 
