4e24d65f1034d947cc2b62c7887082635ddde585
Vitaliy Fedoriv <vitaliy.fedoriv@gmail.com>
2018-06-21 20:45:08 +0300

    create login component

--- src/app/security/login/login.component.spec.ts
+++ src/app/security/login/login.component.spec.ts
@@ -1,14 +1,30 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
+import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {ActivatedRoute, Router} from '@angular/router';
+import {ActivatedRouteStub, RouterStub} from '../../testing/router-stubs';
+import {User} from '../user';
+import {SecurityService} from '../security.service';
+import {HttpClientModule} from '@angular/common/http';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+  let testUser: User;
+  let securityService: SecurityService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      declarations: [ LoginComponent ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      imports: [FormsModule, HttpClientModule],
+      providers: [
+        SecurityService,
+        {provide: Router, useClass: RouterStub},
+        {provide: ActivatedRoute, useClass: ActivatedRouteStub}
+      ]
     })
     .compileComponents();
   }));
@@ -16,6 +32,8 @@ describe('LoginComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
+    testUser = {username: 'name', password: 'password'};
+    securityService = fixture.debugElement.injector.get(SecurityService);
     fixture.detectChanges();
   });
 
--- src/app/security/login/login.component.ts
+++ src/app/security/login/login.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {User} from '../user';
+import {SecurityService} from '../security.service';
+import {AuthClient} from '../authClient';
+import {AuthTokens} from '../authTokens';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +11,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  private errorMessage: string;
+  user: User;
+  private authClient: AuthClient;
+  private authToken: AuthTokens;
 
-  constructor() { }
+  constructor(private securityService: SecurityService, private router: Router) {
+    this.user = <User>{};
+    this.authToken = <AuthTokens>{};
+
+    this.authClient = <AuthClient>{};
+
+    this.authClient.client_id = 'write-client';
+    this.authClient.grant_type = 'password';
+    this.authClient.username = 'admin';
+    this.authClient.password = 'admin123';
+  }
 
   ngOnInit() {
+    this.user.username='write-client';
+    this.user.password='write-client-password1234';
+
+  }
+
+  onSubmit(user: User){
+    this.securityService.getAuthTokens(user, this.authClient).subscribe(
+      new_token => {
+        this.authToken = new_token;
+        console.log(' - AuthToken -');
+        console.log(this.authToken);
+        this.securityService.saveToken(this.authToken);
+      },
+      error => this.errorMessage = <any>error
+    );
+  }
+
+  gotoMain() {
+    this.router.navigate(['/']);
   }
 
 }
