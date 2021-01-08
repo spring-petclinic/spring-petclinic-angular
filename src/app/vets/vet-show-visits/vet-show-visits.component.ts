a1f95f821b88f503ce0b8b580628b148393f0af7
NiJoW <nj@wennesheimer.de>
2021-01-08 13:12:54 +0100

    show list not filtered

--- src/app/vets/vet-show-visits/vet-show-visits.component.ts
+++ src/app/vets/vet-show-visits/vet-show-visits.component.ts
@@ -1,7 +1,11 @@
+import { VetService } from 'app/vets/vet.service';
+import { Vet } from './../vet';
+import { Router, ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
 import { VisitService } from './../../visits/visit.service';
 import { Visit } from './../../visits/visit';
 import { Component, OnInit } from '@angular/core';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-vet-show-visits',
@@ -10,14 +14,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VetShowVisitsComponent implements OnInit {
 
-  plannedVisits: Observable<Visit[]>;
-  pastVisits: Observable<Visit[]>;
+  plannedVisits: Visit[];
+  pastVisits: Visit[];
+  vet: Vet;
+  myDate : string;
 
-  constructor(private visitService: VisitService) { }
-
-  ngOnInit(): void {
-    this.plannedVisits = this.visitService.getVisits();
-    this.pastVisits = this.visitService.getVisits();
+  constructor(private visitService: VisitService,
+              private vetService: VetService,
+              private route: ActivatedRoute,
+              private datePipe: DatePipe) {
+    this.myDate = this.datePipe.transform(this.myDate, 'yyyy-MM-dd');  
   }
 
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.visitService.getVisits().subscribe( res => this.plannedVisits = res)
+    this.visitService.getVisits().subscribe( res => this.pastVisits = res)
+    this.vetService.getVetById(id).subscribe( res => this.vet = res)  
+  }
 }
