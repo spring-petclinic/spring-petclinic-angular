ce81a8922817b8d2071a08cf675dd059cc8a2c75
David Chung <dchung@dchung.com>
2021-01-16 10:27:29 -0600

    Removed unnecessary OnInit() from service

--- src/app/config/appconfig.service.ts
+++ src/app/config/appconfig.service.ts
@@ -1,10 +1,10 @@
-import { Injectable, OnInit } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { map, catchError } from 'rxjs/operators';
 
 @Injectable()
-export class AppConfigService implements OnInit {
+export class AppConfigService {
 
   private envUrl = '/config';
   private configSettings: any = null;
@@ -12,7 +12,6 @@ export class AppConfigService implements OnInit {
   constructor(private http: HttpClient) {}
 
   getAPI(url: string): Observable<any> {
-    console.log(`*** getAPI(${url})`);
     const headers = new Headers({'Content-Type' : 'application/json'});
     return this.http.get(url)
         .pipe(map((response: Response) => response),
@@ -20,7 +19,6 @@ export class AppConfigService implements OnInit {
   }
 
   private handleError(error: Response) {
-    console.log('*** handleError');
     console.log('ERROR::STATUS:::::' + error.status);
     console.log('ERROR::STATUS TEXT:::::' + error.statusText);
     if (error.status === 504 || error.status === 502 || error.status === 503) {
@@ -31,12 +29,10 @@ export class AppConfigService implements OnInit {
   }
 
   get settings() {
-      console.log('*** getSettings');
       return this.configSettings;
   }
 
   public load(): Promise<any> {
-    console.log('*** load()');
     return new Promise((resolve, reject) => {
       this.getAPI(this.envUrl).subscribe((response: any) => {
           console.log('response from the server:::', response);
@@ -46,7 +42,4 @@ export class AppConfigService implements OnInit {
     });
   }
 
-  ngOnInit() {
-    console.log('*** ngOnOnit()');
-  }
 }
