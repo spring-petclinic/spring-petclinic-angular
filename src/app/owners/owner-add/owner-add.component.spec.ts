76294a1ce5a296d72d210359eee31b28c65120e4
simrin051 <simrinjoshi@yahoo.com>
2021-11-04 10:11:53 +0530

    Added test cases for owner add

--- src/app/owners/owner-add/owner-add.component.spec.ts
+++ src/app/owners/owner-add/owner-add.component.spec.ts
@@ -22,28 +22,30 @@
  * @author Vitaliy Fedoriv
  */
 
-import {async, ComponentFixture, TestBed} from '@angular/core/testing';
-import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
 
-import {OwnerAddComponent} from './owner-add.component';
-import {FormsModule} from '@angular/forms';
-import {Router} from '@angular/router';
-import {OwnerService} from '../owner.service';
-import {RouterTestingModule} from '@angular/router/testing';
-import {RouterStub} from '../../testing/router-stubs';
-import {Owner} from '../owner';
-import {Observable, of} from 'rxjs';
-
+import { OwnerAddComponent } from "./owner-add.component";
+import { FormsModule } from "@angular/forms";
+import { Router } from "@angular/router";
+import { OwnerService } from "../owner.service";
+import { RouterTestingModule } from "@angular/router/testing";
+import { RouterStub } from "../../testing/router-stubs";
+import { Owner } from "../owner";
+import { Observable, of } from "rxjs";
+import { By } from "@angular/platform-browser";
+import { OwnersRoutingModule } from "../owners-routing.module";
+import { OwnerListComponent } from "../owner-list/owner-list.component";
 class OwnserServiceStub {
-
   addOwner(owner: Owner): Observable<Owner> {
     return of(owner);
   }
 }
 
-describe('OwnerAddComponent', () => {
+describe("OwnerAddComponent", () => {
   let component: OwnerAddComponent;
   let fixture: ComponentFixture<OwnerAddComponent>;
+  let router: Router;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -51,20 +53,75 @@ describe('OwnerAddComponent', () => {
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
       imports: [FormsModule, RouterTestingModule],
       providers: [
-        {provide: OwnerService, useClass: OwnserServiceStub},
-        {provide: Router, useClass: RouterStub}
-      ]
-    })
-      .compileComponents();
+        { provide: OwnerService, useClass: OwnserServiceStub },
+        { provide: Router, useClass: RouterStub },
+      ],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(OwnerAddComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    router = TestBed.get(Router);
   });
 
-  it('should create OwnerAddComponent', () => {
+  it("should create OwnerAddComponent", () => {
     expect(component).toBeTruthy();
   });
+
+  it("First Name check invalid", async(() => {
+    fixture.whenStable().then(() => {
+      let firstName = component.ownerForm.controls["firstName"];
+      expect(firstName.valid).toBeFalsy();
+      expect(component.ownerForm.valid).toBeFalsy();
+      firstName.setValue("J");
+      expect(firstName.errors["minlength"]).toBeTruthy();
+      firstName.setValue("John");
+      expect(firstName.errors).toBeFalsy();
+    });
+  }));
+
+  it("First Name check invalid", async(() => {
+    fixture.whenStable().then(() => {
+      let lastName = component.ownerForm.controls["lastName"];
+      expect(lastName.valid).toBeFalsy();
+      expect(component.ownerForm.valid).toBeFalsy();
+      lastName.setValue("S");
+      expect(lastName.errors["minlength"]).toBeTruthy();
+      lastName.setValue("Smith");
+      expect(lastName.errors).toBeFalsy();
+    });
+  }));
+
+  it("check form submission is successful", async(() => {
+    fixture.whenStable().then(() => {
+      let firstName = component.ownerForm.form.controls["firstName"];
+      let lastName = component.ownerForm.form.controls["lastName"];
+
+      firstName.setValue("Mary");
+      lastName.setValue("John");
+      let ownerServiceStub = new OwnserServiceStub();
+      let owner = {
+        id: 0,
+        firstName: "Mary",
+        lastName: "John",
+        address: "",
+        city: "",
+        telephone: "",
+        pets: [],
+      };
+      let buttons = fixture.debugElement.queryAll(By.css("button")); // modify here
+      let addButton = buttons[1].nativeElement;
+      addButton.click();
+      let mockRouter = {
+        navigate: jasmine.createSpy("navigate"),
+      };
+
+      spyOn(router, "navigate");
+      spyOn(component, "gotoOwnersList").and.callThrough();
+      component.gotoOwnersList();
+      expect(router.navigate).toHaveBeenCalledWith(["/owners"]);
+    });
+  }));
 });
