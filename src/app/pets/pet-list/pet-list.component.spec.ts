e1e73045a36ae99264bc31b277cbffcc3cb3d7fb
Antoine Rey <antoine.rey@gmail.com>
2021-11-07 19:07:16 +0100

    Fix tslint

--- src/app/pets/pet-list/pet-list.component.spec.ts
+++ src/app/pets/pet-list/pet-list.component.spec.ts
@@ -22,16 +22,16 @@
  * @author Vitaliy Fedoriv
  */
 
-import { ComponentFixture, TestBed, waitForAsync } from "@angular/core/testing";
-import { CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
-import { PetListComponent } from "./pet-list.component";
-import { FormsModule } from "@angular/forms";
-import { PetService } from "../pet.service";
-import { ActivatedRoute, Router } from "@angular/router";
-import { ActivatedRouteStub, RouterStub } from "../../testing/router-stubs";
-import { Pet } from "../pet";
-import { Observable, of } from "rxjs";
+import { PetListComponent } from './pet-list.component';
+import { FormsModule } from '@angular/forms';
+import { PetService } from '../pet.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRouteStub, RouterStub } from '../../testing/router-stubs';
+import { Pet } from '../pet';
+import { Observable, of } from 'rxjs';
 import Spy = jasmine.Spy;
 
 class PetServiceStub {
@@ -40,7 +40,7 @@ class PetServiceStub {
   }
 }
 
-describe("PetListComponent", () => {
+describe('PetListComponent', () => {
   let component: PetListComponent;
   let fixture: ComponentFixture<PetListComponent>;
   let inputPet: Pet;
@@ -67,34 +67,34 @@ describe("PetListComponent", () => {
     component = fixture.componentInstance;
     inputPet = {
       id: 1,
-      name: "Leo",
-      birthDate: "2010-09-07",
-      type: { id: 1, name: "cat" },
+      name: 'Leo',
+      birthDate: '2010-09-07',
+      type: { id: 1, name: 'cat' },
       owner: {
         id: 1,
-        firstName: "George",
-        lastName: "Franklin",
-        address: "110 W. Liberty St.",
-        city: "Madison",
-        telephone: "6085551023",
+        firstName: 'George',
+        lastName: 'Franklin',
+        address: '110 W. Liberty St.',
+        city: 'Madison',
+        telephone: '6085551023',
         pets: null,
       },
       visits: null,
     };
     component.pet = inputPet;
     petService = fixture.debugElement.injector.get(PetService);
-    spy = spyOn(petService, "deletePet").and.returnValue(of(1));
+    spy = spyOn(petService, 'deletePet').and.returnValue(of(1));
 
     fixture.detectChanges();
   });
 
-  it("should create PetListComponent", () => {
+  it('should create PetListComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it("should call deletePet() method", () => {
+  it('should call deletePet() method', () => {
     fixture.detectChanges();
     component.deletePet(component.pet);
-    expect(spy.calls.any()).toBe(true, "deletePet called");
+    expect(spy.calls.any()).toBe(true, 'deletePet called');
   });
 });
