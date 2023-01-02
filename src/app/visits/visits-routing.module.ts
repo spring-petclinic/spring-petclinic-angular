d6c5f7b505c037ba62504c8c4e67af1438773e03
jannisherlt <jannis.herlt@t-online.de>
2023-01-02 12:27:56 +0100

    add delete confirmation

--- src/app/visits/visits-routing.module.ts
+++ src/app/visits/visits-routing.module.ts
@@ -26,10 +26,11 @@ import {VisitListComponent} from './visit-list/visit-list.component';
 import {VisitEditComponent} from './visit-edit/visit-edit.component';
 import {VisitAddComponent} from './visit-add/visit-add.component';
 
+
 const visitRoutes: Routes = [
   {path: 'visits', component: VisitListComponent},
   {path: 'visits/add', component: VisitAddComponent},
-  {path: 'visits/:id/edit', component: VisitEditComponent}
+  {path: 'visits/:id/edit', component: VisitEditComponent},
 
 ];
 
