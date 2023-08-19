e7f73a4876d900840647c7728691ea96df4f47f2
dariusdv <ddariusn@gmail.com>
2023-08-20 01:20:28 +0300

    Angular 16 ready

--- src/app/specialties/spec-resolver.ts
+++ src/app/specialties/spec-resolver.ts
@@ -18,7 +18,7 @@
 
 import {Injectable} from '@angular/core';
 import {Specialty} from './specialty';
-import {Resolve} from '@angular/router';
+
 import {SpecialtyService} from './specialty.service';
 import {Observable} from 'rxjs';
 
@@ -27,7 +27,7 @@ import {Observable} from 'rxjs';
  */
 
 @Injectable()
-export class SpecResolver implements Resolve<Specialty[]> {
+export class SpecResolver  {
 
   constructor(private specialtyService: SpecialtyService) { }
 
